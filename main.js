e2301cdea36b936cbaf24c50d156d6ff3fbadd64
Petr Eremeykin <eremeykin@gmail.com>
2018-12-08 13:25:13 +0300

    Implement LazyGraph with recursion

--- main.js
+++ main.js
@@ -3,18 +3,25 @@ import {parseFunctionArgs} from './fparser.js';
 class LazyGraph {
 
     receiveGraph(graph) {
+        this.graph = graph;
+        this.vertices = {};
         for (let vertex in graph) {
-            console.log("parse: " + vertex)
-            console.log(parseFunctionArgs(graph[vertex]))
+            this.vertices[vertex] = parseFunctionArgs(graph[vertex]);
         }
+        return this;
     }
 
     calcVertex(vertexName) {
-        console.log(arguments);
+        let toCalculate = this.vertices[vertexName];
+        let calculationResults = [];
+        for (let i = 0; i < toCalculate.length; i++) {
+            let vertexResult = this.calcVertex(toCalculate[i].toString());
+            calculationResults.push(vertexResult);
+        }
+        return this.graph[vertexName](...calculationResults);
     }
 }
-
-
-let gObj = {x: (y, z, t) => y + 1, y: () => 1};
-let lg = new LazyGraph();
-lg.receiveGraph(gObj);
+let graphDefinition = {x: (y, z, t) => 1 + y + z + 3 * t, y: () => 1, z: () => 3, t: (y, z) => 5+y+z};
+let lazyGraph = new LazyGraph();
+let x = lazyGraph.receiveGraph(graphDefinition).calcVertex('x');
+console.log("x = "+x);

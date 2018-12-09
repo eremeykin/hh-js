4883c7d47b1b45b2c7de9a2d04e04c9ae6c73c91
Petr Eremeykin <eremeykin@gmail.com>
2018-12-09 09:43:16 +0300

    Refactor lazy_graph.js code

--- lazy_graph.js
+++ lazy_graph.js
@@ -13,35 +13,29 @@ export class LazyGraph {
 
     calcVertex(vertex) {
         let vertexStack = [vertex]; // current vertex
-        let  indexStack = [0]; // index of next vertex
+        let  indexStack = [0]; // index of next child vertex to visit
         let resultStack = [];
-        let nextIntex =0;
 
-        let c =0;
-        while(vertexStack.length>0){
-            // c++;
-            // if (c>100){
-            //     return -1;
-            // }
-            // console.log(vertexStack);
+        while(vertexStack.length>0){ // recursion is evil
             let currentVertex = vertexStack[vertexStack.length -1];
-            let nextIntex = indexStack[indexStack.length-1];
+            let nextIndex = indexStack[indexStack.length-1];
 
             if (typeof this.vertices[currentVertex] == 'undefined'){
-                    throw new Error("The received graph doesn't define vertex: " + currentVertex);
+                    throw new Error("The received graph doesn't define the vertex: " + currentVertex);
             }
 
-            if (nextIntex + 1 > this.vertices[currentVertex].length){
+            if (nextIndex + 1 > this.vertices[currentVertex].length){ // all child vertices has been visited
                 let currentArgs = [];
                 for(let i=0; i<this.vertices[currentVertex].length; i++){
-                    currentArgs.push(resultStack.pop());
+                    currentArgs.push(resultStack.pop()); // pop arguments from stack
                 }
-                let res = this.graph[currentVertex](...currentArgs);
+                let res = this.graph[currentVertex](...currentArgs); // evaluate function
+                this.graph[currentVertex] = ()=>res; // cache result
                 resultStack.push(res);
                 vertexStack.pop();
                 indexStack.pop();
             }else{
-                let nextVertex = this.vertices[currentVertex][nextIntex];
+                let nextVertex = this.vertices[currentVertex][nextIndex];
                 if (vertexStack.indexOf(nextVertex)>-1){
                     throw new Error("There is a loop in received graph");
                 }
@@ -53,21 +47,3 @@ export class LazyGraph {
         return resultStack[0];
     }
 }
-
-let lazyGraph = new LazyGraph();
-let graphDefinition = {
-    x1: (x2, x3, x4) => 1,
-    x2: (x5, x6) => 2,
-    x5: (x10) => 3,
-    x10: () => 4,
-    x6: (x11, x12) => 5,
-    x11: () => 6,
-    x12: () => 7,
-    x3: (x7, x8, x9) =>8,
-    x7: () => 9,
-    x8: () => 10,
-    x9: () => 11,
-    x4: () => 12
-};
-let calcResult = lazyGraph.receiveGraph(graphDefinition).calcVertex('x1');
-
